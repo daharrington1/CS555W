2ddc85f49ffcfd0ae73b64787e51d97134719299
Chengyi Zhang <61096603+MarquisBan@users.noreply.github.com>
2020-06-16 17:22:12 +0800

    Update usrun.py

--- usrun.py
+++ usrun.py
@@ -1,24 +1,35 @@
 # The UserStory program by developer&tester Chengyi Zhang
 
-from Main import individuals_from_db, families_from_db
+# tool
+def IDtoINDI(individuals_from_db):
+    ans = dict()
+    for one in individuals_from_db:
+        ans.setdefault(one['INDI'], one)
+    return ans
+
 
 # US24 List Children with Divorced Parents
-for family in families_from_db:
-    if('DIV' in family):
-        if('CHIL' in family):
-            for one in family['CHIL']:
-                for p in individuals_from_db:
-                    if(p['INDI'] == one):
-                        print("ANOMALY: Child " + p['NAME'] + " has divorced parents")
+def children_parents_divorced(families_from_db, individuals_from_db):
+    id_indi = IDtoINDI(individuals_from_db)
+    for family in families_from_db:
+        if ('DIV' in family):
+            if ('CHIL' in family):
+                for one in family['CHIL']:
+                    print("ANOMALY: INDIVIDUAL: Child " + id_indi[one]['NAME'] + " has divorced parents")
+
 
 # US32 List People Having the Same Birthday
-bds = dict()
-for one in individuals_from_db:
-    t = bds.get(one['BIRT'],[])
-    bds[one['BIRT']].append(one['NAME'])
-for date, names  in bds.items():
-    if(len(names)>1):
-        print("ANOMALY: " + ' and '.join(names) + " have the same birthday")
+def people_same_birthday(individuals_from_db):
+    bds = dict()
+    for one in individuals_from_db:
+        s = str(one['BIRT'][0]) + '/' + str(one['BIRT'][1]) + '/' + str(one['BIRT'][2])
+        n = one['NAME']
+        bds.setdefault(s, [])
+        bds[s].append(n)
+    for date, names in bds.items():
+        if len(names) > 1:
+            print("ANOMALY: INDIVIDUAL: " + ', '.join(names[:-1]) + ', and ' +
+                  names[-1] + " have the same birthday " + date)
 
 
 '''
@@ -26,4 +37,3 @@ TEMPORARY
 print(individuals_from_db[0])
 print(families_from_db[0])
 '''
-
