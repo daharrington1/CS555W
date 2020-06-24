1fb773a8a6c8c60602cbea53ccf709f13ce28e92
daharrington1 <51126382+daharrington1@users.noreply.github.com>
2020-06-24 11:20:53 -0400

    Update run_utests.sh

--- run_utests.sh
+++ run_utests.sh
@@ -1,16 +1,5 @@
-echo "UserStory23/29 Tests:"
-python3 -m unittest GroupGedcomTest/DBInterfaceTest.py
-echo ""
-python3 -m unittest GroupGedcomTest/TablePrinterTest.py
-echo ""
-echo "UserStory17 Tests:"
-python3 -m unittest GroupGedcomTest/US17Test.py
-echo ""
-echo "UserStory18 Tests:"
-python3 -m unittest GroupGedcomTest/US18Test.py
-echo ""
-echo "UserStory 24/32 Tests:"
-python3 -m unittest GroupGedcomTest/usrun_test.py
-
-echo "UserStory 01/07 Tests:"
-python3 -m unittest GroupGedcomTest/project4Test.py
+for file in `ls GroupGedcomTest/*.py`
+do
+echo $file ":"
+python3 -m unittest $file
+done
